eba983868903ef4f43f065335e3295681bbb459b
Hsuchein <Hsuchein0126@outlook.com>
2024-04-07 00:52:56 +0800

    Transformer

--- module_test.py
+++ module_test.py
@@ -1,9 +1,10 @@
 from tinkermodellor.build import GMXSystem
 from tinkermodellor.build import TinkerSystem
+from tinkermodellor.build import Transformer
 
 if __name__ == '__main__':
 
-    control = 1
+    control = 2
 
     if control == 0:
         gmx = GMXSystem()
@@ -11,5 +12,13 @@ if __name__ == '__main__':
             r'/home/wayne/quanmol/TinkerModellor/example/gromacs.top')
     elif control == 1:
         tk = TinkerSystem()
-        tk.read_from_tinker(r'/home/wayne/quanmol/TinkerModellor/example/gromacs.xyz')
+        tk.read_from_tinker(r'/home/ayne/quanmol/TinkerModellor/example/gromacs.xyz')
         tk.write(r'/home/wayne/quanmol/TinkerModellor/example/tinker.xyz')
+    elif control == 2:
+        gmx = GMXSystem()
+        gmx.read_gmx_file(r'/home/wayne/quanmol/TinkerModellor/example/gromacs.gro', \
+            r'/home/wayne/quanmol/TinkerModellor/example/gromacs.top')
+        transformer = Transformer()
+        tk = transformer(gmx)
+        tk.write(r'/home/wayne/quanmol/TinkerModellor/example/tinker.xyz')
+    
